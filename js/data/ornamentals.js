db643b66360c4bcb42c5200ad7eeba5726ac63c2
Kenny Wilson <kenny@digital-inflection.com>
2017-10-19 15:40:24 -0400

    init commit

---
+++ js/data/ornamentals.js
@@ -0,0 +1,18 @@
+var txt = '{"ornamentals":[' +
+'{"commonName":"Sycamore","scientificName":"American planetree","image":["Sycamore.jpg","Sycamore2.jpg","Sycamore3.jpg","Sycamore4.jpg","Sycamore5.jpg"],"imageDescription":["Plant - The American planetree, also known as sycamore, is a large, wide-spreading tree that is often found growing naturally in moist soils along rivers and streams.","Bark - American planetree bark is distinguished by red to gray scaly patches that peel back to reveal creamy white layers beneath.","Twig - American planetree leaves are arranged alternately along the stem.","Leaf - The large leaves of American planetree have 3-5 triangular lobes.","Fruit - Small, round, spike covered balls that start out light green, but turn dark brown before opening and dropping their seeds."] },' +
+
+'{"commonName":"Arborvitae","scientificName":"Thuja oocidentalis","image":["Arborvitae.jpg","Arborvitae2.jpg"],"imageDescription":["Arborvitae is a small to large evergreen shrub commonly found in the home landscape.","Description 2"] },' +
+
+'{"commonName":"Tree","scientificName":"TreeusMaximus","image":["Tree.jpg"],"imageDescription":["This is a tree"] },' +
+
+'{"commonName":"Rose","scientificName":"TreeusMaximus","image":["Rose.jpg"],"imageDescription":["This is a tree"] },' +
+
+'{"commonName":"Tulip","scientificName":"TreeusMaximus","image":["Tulip.jpg"],"imageDescription":["This is a tree"] },' +
+
+'{"commonName":"Dahlia","scientificName":"DahliasMaximus","image":["Dahlia.jpg"],"imageDescription":["This is a daliah"] },' +
+
+'{"commonName":"Daffodil","scientificName":"DaffodilMaximus","image":["Daffodil.jpg"],"imageDescription":["This is a daffodil"] },' +
+
+'{"commonName":"Susan","scientificName":"DaffodilMaximus","image":["Susan.jpg"],"imageDescription":["This is a susan"] },' +
+
+'{"commonName":"Sunflower","scientificName":"FlowerousMinimus" ,"image":["Sunflower.jpg"],"imageDescription":["This is a pretty flower"]}]}';
